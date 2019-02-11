8a2e180cba882f67ca62e592c733826ec79c9390
Andika Kurniawan <andikadevs@gmail.com>
2019-02-11 11:07:56 +0700

    shorten code, using checking internet connection class

--- app/src/main/java/insure/onoff/data/repository/sample/SampleRepository.kt
+++ app/src/main/java/insure/onoff/data/repository/sample/SampleRepository.kt
@@ -7,6 +7,7 @@ package insure.onoff.data.repository.sample
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import insure.onoff.common.connection.ConnectivityReceiver
 import insure.onoff.core.calling_api.APIList
 import insure.onoff.core.calling_api.RetrofitManager
 import insure.onoff.data.models.sample.Request
@@ -31,15 +32,17 @@ class SampleRepository() {
     fun register(request: Request) : LiveData<Response> {
         val authResponse : MutableLiveData<Response> = MutableLiveData();
 
-        apiList.displaySomeResponse(request).enqueue(object : Callback<Response> {
-            override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
-                authResponse.value = response.body();
-            }
+        if(ConnectivityReceiver.isConnected) {
+            apiList.displaySomeResponse(request).enqueue(object : Callback<Response> {
+                override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
+                    authResponse.value = response.body();
+                }
 
-            override fun onFailure(call: Call<Response>, t: Throwable) {
-                Log.e("TAG", "onFailure");
-            }
-        })
+                override fun onFailure(call: Call<Response>, t: Throwable) {
+                    Log.e("TAG", "onFailure");
+                }
+            })
+        }
 
         return authResponse;
     }
